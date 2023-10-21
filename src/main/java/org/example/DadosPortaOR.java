6299ec9b7beda3426b7af9c8af44bae67aac59f3
08786218654 <leandrosena@lampp-it.com.br>
2023-10-21 15:39:37 -0300

    Init.

---
+++ src/main/java/org/example/DadosPortaOR.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DadosPortaOR {
+    private int[][] matriz = {{1,1,1},{1,-1,1},{1,-1,1},{-1,-1,-1}};
+
+    public DadosPortaOR() {
+
+    }
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+}
